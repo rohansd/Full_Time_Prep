676e10df0b8f817be73dab4b7a5b83ba902395b7
rohansd <rohanshivajirao.deshmukh@sjsu.edu>
2022-03-05 15:33:06 -0800

    cosmatic changes

--- client/script.js
+++ client/script.js
@@ -1,16 +1,35 @@
-const body = document.getElementById("main");
+const profile = document.getElementById("profile");
 
 const APIURL = "https://api.github.com/users/";
 
 async function user(){
-    let response = await fetch(APIURL+'florinpop17');
-    let data = await response.json();
-    // console.log(data);
-    body.innerHTML = `
-    <div>
-        <h1>${data.name}</h1>
-        <h4>${data.bio}</h4>
-        <img src="${data.avatar_url}"></img>
-    </div>`;
+    let username = document.getElementById("username");
+    let response = await fetch(APIURL+username.value);
+    
+    if(response.status === 200){
+        let data = await response.json();
+        profile.innerHTML = `
+        <div class="profile card mb-3" style="width: 540px;">
+            <div class="row g-0">
+                <div class="col-md-4">
+                    <img src="${data.avatar_url}" class="img-fluid rounded-start" alt="...">
+                </div>
+                <div class="col-md-8">
+                    <div class="card-body">
+                        <h5 class="card-title">${data.name}</h5>
+                        <p class="card-text">${data.bio}</p>
+                    </div>
+                </div>
+            </div>  
+        </div>`;
+    }
+    else{
+        profile.innerHTML = `
+        <div class="profile card mb-3" style="width: 540px;">
+            <div class="row g-0">
+                Invalid User :(
+            </div>  
+        </div>`;
+    }
+    username.value = "";
 }
-// user();
